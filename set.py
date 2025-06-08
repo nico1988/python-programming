a3a79d9c7d87c701c10a5035fa579377594d3aa1
nico1988 <20182726@csuft.edu.cn>
2025-06-08 22:57:46 +0800

    feat: set demos

---
+++ set.py
@@ -0,0 +1,37 @@
+# set demos
+set_demo = set()
+# add elements to the set
+set_demo.add(1)
+set_demo.add(2)
+set_demo.add(3)
+set_demo.add(4)
+
+# add a duplicate element
+set_demo.add(1)
+
+# add a string element
+set_demo.add("hello")
+
+# print the set
+print(set_demo)
+
+# loop through the set
+for item in set_demo:
+    print(item)
+
+# check if an element is in the set
+if 1 in set_demo:
+    print("1 is in the set")
+else:
+    print("1 is not in the set")  
+
+# check if an element is in the set using contains
+print(set_demo.__contains__(1))  
+
+# size
+print("Size of the set:", len(set_demo))
+
+# clear the set
+set_demo.clear()
+print("Set after clearing:", set_demo)
+print("Length of the set after clearing:", len(set_demo))
