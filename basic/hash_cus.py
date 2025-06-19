3fb84a3c9f03f7d5c9529b9a3b17d28e3ae1dd56
nico1988 <20182726@csuft.edu.cn>
2025-06-19 08:45:20 +0800

    update logger

--- basic/hash_cus.py
+++ basic/hash_cus.py
@@ -11,5 +11,16 @@ def hash_demo():
     # Log the hash
     logger.info("SHA-256 Hash: %s", sha256_hash)
 
+    # md5 hash hexdigest
+    logger.info("MD5 Hash: %s", hashlib.md5(example_string.encode()).hexdigest())
+    # md5 hash digest
+    logger.info("MD5 Digest: %s", hashlib.md5(example_string.encode()).digest())
+
+    # sha1 hash
+    logger.info("SHA-1 Hash: %s", hashlib.sha1(example_string.encode()).hexdigest())
+
+    # sha512 hash
+    logger.info("SHA-512 Hash: %s", hashlib.sha512(example_string.encode()).hexdigest())
+
 if __name__ == "__main__":
     hash_demo()
