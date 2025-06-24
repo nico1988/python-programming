61ba2281355bdb6925d262c6f0c0e432f96d5602
nico1988 <20182726@csuft.edu.cn>
2025-06-24 22:52:31 +0800

    update module

---
+++ package_cc/__init__.py
@@ -0,0 +1,4 @@
+if __name__ == "__main__":
+    print("This is the package_cc module. It can be used to load Python modules from specified paths.")
+else:
+    print("package_cc is imported. You can use it to load Python modules from specified paths.")
---
+++ package_cc/cc2.py
@@ -0,0 +1,3 @@
+def cc2():
+    print("This is cc2.py in package_cc")
+    return "cc2"
